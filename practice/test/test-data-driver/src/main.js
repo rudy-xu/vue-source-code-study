d9ea5d05bb46b22618f3bb9e01f8b5c4dab199c5
cade <13024334591@163.com>
2021-07-17 16:55:07 +0800

    create component...

--- practice/test/test-data-driver/src/main.js
+++ practice/test/test-data-driver/src/main.js
@@ -1,20 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import App from './App'
+import App from './App'
 
 // Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render(h) {
+    // tag: App -> component
+    return h(App)
+  },
 
   mounted () {
-    console.log(this._data.message)
-    console.log(this.message)
+    // console.log(this._data.message)
+    // console.log(this.message)
   },
 
   // render(createElement) {
+  //   // tag: div
+  //   // data: attrs
+  //   // text: this.message
   //   return createElement('div', {
   //     attrs: {
   //       id: 'app1'
@@ -22,11 +29,11 @@ new Vue({
   //   }, this.message);
   // },
 
-  data () {
-    return {
-      message: 'Hello Vue!'
-    }
-  } 
+  // data () {
+  //   return {
+  //     message: 'Hello Vue!'
+  //   }
+  // } 
   // components: { App },
   // template: '<App/>'
 })
